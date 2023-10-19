b2edb705713e5e1ba200e4d42029ad62d0f4f673
Shahied Rustin <97807830+snowyru@users.noreply.github.com>
2023-10-19 22:09:03 +0200

    update comments added User content

--- src/section1/MonthlyRevenue.java
+++ src/section1/MonthlyRevenue.java
@@ -20,18 +20,23 @@ public class MonthlyRevenue {
             arrRevenue[i] = (int)(Math.random()*100);
         
         } // Populate the array with 30 random numbers 
-        System.out.println("The mean is: " + mean(arrRevenue) ); // Print the mean
-        System.out.println("The median is: " + Median(arrRevenue) ); // Print the Median
-        System.out.println("The mode is: " + Mode(arrRevenue) ); // Print the Mode
+            System.out.println("The monthly paypment array is : ");
+                for (int i = 0; i < arrRevenue.length; i++){
+                    System.out.print(arrRevenue[i] + " ; " ); // Print the array
+                }
+            System.out.println("");
+            System.out.println("The mean is: " + Mean(arrRevenue) ); // Print the Mean
+            System.out.println("The median is: " + Median(arrRevenue) ); // Print the Median
+            System.out.println("The mode is: " + Mode(arrRevenue) ); // Print the Mode
     }
 
-    public static int mean(int[] arrRevenue){
+    public static int Mean(int[] arrRevenue){
         if (arrRevenue.length == 0 ){
             return 0;
         }
         int sum = 0;
 
-            for (int num : arrRevenue){
+            for (int num : arrRevenue){ //for each loop 
                 sum +=num;
             }
         
@@ -46,7 +51,7 @@ public class MonthlyRevenue {
         Arrays.sort(arrRevenue);
 
         if (arrRevenue.length %2 == 0){
-            return (arrRevenue[arrRevenue.length / 2 - 1] + arrRevenue[arrRevenue.length / 2]) / 2;
+            return (arrRevenue[(arrRevenue.length / 2)- 1] + arrRevenue[arrRevenue.length / 2]) / 2;
         } else {
             return arrRevenue[arrRevenue.length / 2];
         }
@@ -56,14 +61,14 @@ public class MonthlyRevenue {
         if (arrRevenue.length == 0 ){
             return 0;
         }
-        Map<Integer, Integer> countMap = new HashMap<>();
-        for (int num : arrRevenue) {
+        Map<Integer, Integer> countMap = new HashMap<>(); // Creates a hashmap
+        for (int num : arrRevenue) { // For each loop to add every value in the array to the map then assigns a count value to record how many times the same value is read
             countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
 
         int mode = arrRevenue[0];
         int maxCount = 0;
-        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) { // for each loop to compare the maxCount value to the count value of every entry in the hashmap. The maxCount is update to the new key of the map if necessary
             if (entry.getValue() > maxCount) {
                 maxCount = entry.getValue();
                 mode = entry.getKey();
--- src/section1/User.java
+++ src/section1/User.java
@@ -1,5 +1,27 @@
 package section1;
 
 public class User {
-    
+    private String username; // Creating the attributes
+    private String password; // Encapsulating the attributes
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() { //Getter for the username
+        return username;
+    }
+
+    public void setUsername(String username) { //Setter for the username
+        this.username = username;
+    }
+
+    public String getPassword() { //Getter for the password
+        return password;
+    }
+
+    public void setPassword(String password) { //Setter for the password
+        this.password = password;
+    }
 }
