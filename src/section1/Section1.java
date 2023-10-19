07a1f3b3201d735c8bec5db0043c6df0b34aa7b9
230500226 <230500226@mycput.ac.za>
2023-10-19 10:33:54 +0200

    init

---
+++ src/section1/Section1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package section1;
+
+/**
+ *
+ * @author 230500226
+ */
+public class Section1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+    }
+    
+}
